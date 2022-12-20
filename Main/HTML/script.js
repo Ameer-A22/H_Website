a35c8b17c88c7724e38f49ab13949972f35a7fc4
Ameer <117360182+Ameer-A22@users.noreply.github.com>
2022-12-20 13:42:55 +0000

    Delete Main directory

--- Main/HTML/script.js
+++
@@ -1,8 +0,0 @@
-function openNav() {
-    document.getElementById("mysidenav").style.width = "250px";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("mysidenav").style.width = "0";
-  }
