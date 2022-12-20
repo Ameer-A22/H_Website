6fbd16e8d5af69b9f9d4ad4e70ac5ee4d66d01d2
Ameer <117360182+Ameer-A22@users.noreply.github.com>
2022-12-20 13:42:43 +0000

    Add files via upload

---
+++ HTML/script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mysidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mysidenav").style.width = "0";
+  }
